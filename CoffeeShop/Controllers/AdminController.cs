7f0e1017b71a678125faf67531ed13305249bb62
Eliraas1 <eliraas1@ac.sce.ac.il>
2021-12-27 11:40:14 +0200

    changed variable names in Admin Controller.EditCoffee

--- CoffeeShop/Controllers/AdminController.cs
+++ CoffeeShop/Controllers/AdminController.cs
@@ -20,9 +20,11 @@ namespace CoffeeShop.Controllers
             
             List<coffee> coffeList = (new coffeeDal()).Coffee.ToList<coffee>();
 
-            Dictionary<string, object> dict = new Dictionary<string, object>();
-            dict.Add("users",users);
-            dict.Add("coffee", coffeList);
+            Dictionary<string, object> dict = new Dictionary<string, object>
+            {
+                { "users", users },
+                { "coffee", coffeList }
+            };
 
             return View(dict);
         }
@@ -36,8 +38,10 @@ namespace CoffeeShop.Controllers
             List<user> users = (new UserDal()).Users.ToList<user>();
             users = (from x in userDal.Users where x.name.Contains(searchedValue) select x).ToList<user>();
 
-            Dictionary<string, object> dict = new Dictionary<string, object>();
-            dict.Add("users", users);
+            Dictionary<string, object> dict = new Dictionary<string, object>
+            {
+                { "users", users }
+            };
 
             return View("Index", dict);
             
@@ -48,38 +52,38 @@ namespace CoffeeShop.Controllers
         public ActionResult EditCoffee()
         {
             string newPrice = Request.Form["newPrice"];
-            string key = null;
-            if (Request.Form.AllKeys.Length == 0)
-                key = null;
-            else
-                key = Request.Form.AllKeys[2];
+            string coffeeKey = null;
+            if (Request.Form.AllKeys.Length != 0)
+                coffeeKey = Request.Form.AllKeys[2];
 
             string newName = Request.Form["newName"];
 
-            if (key == null)
+            if (coffeeKey == null)
                 return View("Index");
 
             coffeeDal cd = new coffeeDal();
-            coffee removed = cd.Coffee.Find(key);
-            cd.Coffee.Remove(removed);
+            coffee updatedCoffee = cd.Coffee.Find(coffeeKey);
+            cd.Coffee.Remove(updatedCoffee);
             cd.SaveChanges();
             if (!newName.Equals(""))
-                removed.name = newName;
+                updatedCoffee.name = newName;
 
             if (!newPrice.Equals(""))
-                removed.price = newPrice;
+                updatedCoffee.price = newPrice;
 
             
-            cd.Coffee.Add(removed);
+            cd.Coffee.Add(updatedCoffee);
             cd.SaveChanges();
 
             List<user> users = (new UserDal()).Users.ToList<user>();
 
             List<coffee> coffeList = (new coffeeDal()).Coffee.ToList<coffee>();
 
-            Dictionary<string, object> dict = new Dictionary<string, object>();
-            dict.Add("users", users);
-            dict.Add("coffee", coffeList);
+            Dictionary<string, object> dict = new Dictionary<string, object>
+            {
+                { "users", users },
+                { "coffee", coffeList }
+            };
 
 
             return View("Index",dict);
