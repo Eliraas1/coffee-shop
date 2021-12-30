cb03674426f9f0380f5bdff9a200e57b971402c1
Eliraas1 <eliraas1@ac.sce.ac.il>
2021-12-30 14:57:34 +0200

    selection option to sort

--- CoffeeShop/Controllers/HomeController.cs
+++ CoffeeShop/Controllers/HomeController.cs
@@ -25,15 +25,55 @@ namespace CoffeeShop.Controllers
 
             return View(sort);
         }
-         public ActionResult AscByPop()
+        public ActionResult Sort()
         {
-            List<Drink> d = db.Drink.ToList<Drink>();
+            string userSelection = Request.Form["sortSelect"];
+            List<Drink> drinks = db.Drink.ToList<Drink>();
 
-            IEnumerable<Drink> Isort = d.OrderByDescending(drink => drink.popular);
-            List<Drink> sort = Isort.ToList();
-            return View("Menu", sort);
+            return View("Menu", sortActionHelper(userSelection, drinks));
         }
-        
 
+        //Sort Helpers
+        public List<Drink> sortActionHelper(string select, List<Drink> drink)
+        {
+            switch(select){
+                case "least-popular": return sortByPopulareAsc(drink);
+                case "most-popular": return sortByPopulareDesc(drink);
+                case "price-asc": return sortByPriceAsc(drink);
+                case "price-desc": return sortByPriceDesc(drink);
+                case "name-asc": return sortByNameAsc(drink);
+                case "name-desc": return sortByNameDesc(drink);
+                case "in-stock": return sortByStock(drink);
+                default: return sortByPopulareAsc(drink);
+            }
+        }
+        public List<Drink> sortByPriceAsc(List<Drink> drinks)
+        {
+            return drinks.OrderBy(drink => drink.price).ToList();
+        }
+        public List<Drink> sortByPriceDesc(List<Drink> drinks)
+        {
+            return drinks.OrderByDescending(drink => drink.price).ToList();
+        }
+        public List<Drink> sortByPopulareDesc(List<Drink> drinks)
+        {
+            return drinks.OrderByDescending(drink => drink.popular).ToList();
+        }
+        public List<Drink> sortByPopulareAsc(List<Drink> drinks)
+        {
+            return drinks.OrderBy(drink => drink.popular).ToList();
+        }
+        public List<Drink> sortByNameAsc(List<Drink> drinks)
+        {
+            return drinks.OrderBy(drink => drink.name).ToList();
+        }
+        public List<Drink> sortByNameDesc(List<Drink> drinks)
+        {
+            return drinks.OrderByDescending(drink => drink.name).ToList();
+        }
+        public List<Drink> sortByStock(List<Drink> drinks)
+        {
+            return drinks.OrderByDescending(drink => drink.amount).ToList().Where(d=>d.amount>0).ToList();
+        }
     }
 }
